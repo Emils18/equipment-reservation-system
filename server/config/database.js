12c9916d10d26960a3e0942d38cceaf683956fde
Mio Monda <emeliomondares14@gmail.com>
2026-09-24 01:16:43 +0800

    Fix TiDB Cloud SSL database connection

--- server/config/database.js
+++ server/config/database.js
@@ -8,16 +8,28 @@ require("dotenv").config();
 
 // Create a reusable database connection pool
 const database = mysql.createPool({
+
   host: process.env.DB_HOST,
+
   port: process.env.DB_PORT,
+
   user: process.env.DB_USER,
+
   password: process.env.DB_PASSWORD,
+
   database: process.env.DB_NAME,
 
-  // Allows multiple users to use the system at once
+  ssl: {
+    rejectUnauthorized: true,
+  },
+
   waitForConnections: true,
+
   connectionLimit: 10,
+
   queueLimit: 0,
+
 });
+
 
 module.exports = database;
